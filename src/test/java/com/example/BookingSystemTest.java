72400f8d547d030e761f05b73b528553325d8e49
danne <xsz300@gmail.com>
2026-01-28 18:17:16 +0100

    cvsSource for parameterized test bookRoom

--- src/test/java/com/example/BookingSystemTest.java
+++ src/test/java/com/example/BookingSystemTest.java
@@ -2,11 +2,14 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,12 +26,17 @@ class BookingSystemTest {
     BookingSystem bookingSystem;
 
 
-    @Test
-    void bookRoomThrowExeptionWithNullTimeOrId(){
+    @ParameterizedTest
+    @CsvSource({
+            ",,", //all input null
+            ", 2026-01-28T11:00, 2026-01-28T12:00", //roomid null
+            "Room1, ,2026-01-28T12:00", //Start time null
+            "Room1, 2026-01-28T12:00, " //end time null
+    })
+    void bookRoomThrowExeptionWithNullInput(String roomId, LocalDateTime start, LocalDateTime end){
         assertThrows(IllegalArgumentException.class, () -> {
-            bookingSystem.bookRoom("", null, null);
+            bookingSystem.bookRoom(roomId, start, end);
         });
-        //TODO Make parametirized test so all branches are tested
     }
 
     @Test
