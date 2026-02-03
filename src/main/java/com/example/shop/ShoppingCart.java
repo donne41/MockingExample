4cd23e5aeb0f0e6a646080f016be97cc17a8a7f3
danne <xsz300@gmail.com>
2026-02-03 19:04:42 +0100

    removeProduct and list implemented in class with corresponding tests

--- src/main/java/com/example/shop/ShoppingCart.java
+++ src/main/java/com/example/shop/ShoppingCart.java
@@ -1,6 +1,21 @@
 package com.example.shop;
 
+import java.util.ArrayList;
+
 public class ShoppingCart {
+
+    ArrayList<String> basket = new ArrayList<>();
+
+
     public void addProduct(String productName) {
+        basket.add(productName);
+    }
+
+    public void removeProduct(String productName) {
+        basket.remove(productName);
+    }
+
+    public ArrayList<String> getProducts() {
+        return basket;
     }
 }
--- src/test/java/com/example/shop/ShoppingCartTest.java
+++ src/test/java/com/example/shop/ShoppingCartTest.java
@@ -2,24 +2,33 @@ package com.example.shop;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class ShoppingCartTest {
 
-    @Mock
-    ShoppingCart cart;
 
 
     @Test
-    void addProductShouldBeSaved() {
+    void addProductShouldBeSavedToList() {
+        ShoppingCart cart = new ShoppingCart();
 
         cart.addProduct("milk");
 
-        Mockito.verify(cart,
-                        Mockito.times(1))
-                .addProduct("milk");
+        var result = cart.getProducts();
+        assertThat(result).contains("milk");
+    }
+
+    @Test
+    void removeProductShouldDeleteProductFromList() {
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.addProduct("milk");
+        cart.removeProduct("milk");
+
+        var result = cart.getProducts();
+        assertThat(result).doesNotContain("milk");
     }
 }
